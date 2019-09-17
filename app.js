7c5afe109ef38349cb8f3d6d8c8fed33ff99cf87
CN72 <cn72@CN72.local>
2019-09-17 16:00:17 +0100

    added figlet

--- app.js
+++ app.js
@@ -2,12 +2,17 @@ const {getWeather} = require('./getWeather')
 const {getLocation} = require('./getLocation')
 const colors = require('colors')
 const figlet = require('figlet')
+const {promisify} = require('util')
+
+const promisifiedFiglet = promisify(figlet)
+
 
 const main = async (place) => {
     const location = await getLocation(place)
     const weather = await getWeather(location)
     const celsius = Math.round((weather.temperature-32)*5/9)
-
+    const data = await promisifiedFiglet("Weather")
+    console.log(data)
     console.log(`The temperature in ${location.name.green} is ${colors.red(celsius)}°C and the probability of rain is ${colors.cyan(weather.precipProbability*100)}%`)
 }
 main()
