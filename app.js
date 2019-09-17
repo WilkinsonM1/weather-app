dcccffdbfc3a313360238fa2875dacc245da686c
CN72 <cn72@CN72.local>
2019-09-17 11:34:13 +0100

    updated git ignore file

--- app.js
+++ app.js
@@ -7,6 +7,6 @@ request({url:"https://api.darksky.net/forecast/d4b693d843f9b93d20786aae043796d4/
         console.log(error)
     }else{
         const data = JSON.parse(response.body)
-        console.log(data.currently.temperature)
+        console.log({temp: data.currently.temperature})
     }
 })
