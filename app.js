d6797af010e19daa63c9718768bb83cf2ee675c8
CN72 <cn72@CN72.local>
2019-09-17 15:39:56 +0100

    added colors

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const {getWeather} = require('./getWeather')
 const {getLocation} = require('./getLocation')
+const colors = require('colors')
+const figlet = require('figlet')
 
 const main = async (place) => {
     const location = await getLocation(place)
     const weather = await getWeather(location)
     const celsius = Math.round((weather.temperature-32)*5/9)
 
-    console.log(`The temperature in ${location.name} is ${celsius}°C and the probability of rain is ${weather.precipProbability*100}%`)
+    console.log(`The temperature in ${location.name.green} is ${colors.red(celsius)}°C and the probability of rain is ${colors.cyan(weather.precipProbability*100)}%`)
 }
 main()
