7bd204d6fd746a69fc6c1580c89faee4272671de
CN72 <cn72@CN72.local>
2019-09-17 11:26:25 +0100

    new

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
 const request = require('request')
 
 const apikey = "d4b693d843f9b93d20786aae043796d4"
+
+request({url:"https://api.darksky.net/forecast/d4b693d843f9b93d20786aae043796d4/37.8267,-122.4233" }, (error,response) =>{
+    if(error){
+        console.log(error)
+    }else{
+        const data = JSON.parse(response.body)
+        console.log(data.currently.temperature)
+    }
+})
